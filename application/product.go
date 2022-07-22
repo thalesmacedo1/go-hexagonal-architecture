f15381188270d0e99dcbaa1b7a251d148758e50b
Thales Macêdo <thalesmacedo1@live.com>
2022-07-22 16:42:41 -0300

    created product interface and consts

---
+++ application/product.go
@@ -0,0 +1,16 @@
+package application
+
+type ProductInterface interface {
+	isValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
