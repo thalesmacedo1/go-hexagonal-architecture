6892eb7436996e8c9c2ec8bd1230975643af5cec
Thales Macêdo <thalesmacedo1@live.com>
2022-07-22 17:16:15 -0300

    created product struct and methods

--- application/product.go
+++ application/product.go
@@ -1,7 +1,9 @@
 package application
 
+import "errors"
+
 type ProductInterface interface {
-	isValid() (bool, error)
+	IsValid() (bool, error)
 	Enable() error
 	Disable() error
 	GetID() string
@@ -14,3 +16,38 @@ const (
 	DISABLED = "disabled"
 	ENABLED  = "enabled"
 )
+
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
+
+// func (p *Product) IsValid() (bool, error) {}
+
+func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
+	return errors.New("price must be greater than zero to enable the product")
+}
+
+// func (p *Product) Disable() error {}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
