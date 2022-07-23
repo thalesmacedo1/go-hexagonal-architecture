3867d8878f719fb6785c8d66d71ef8bebfd0a602
Thales Macêdo <thalesmacedo1@live.com>
2022-07-23 15:53:17 -0300

    created test for enable method

---
+++ application/product_test.go
@@ -0,0 +1,18 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/thalesmacedo1/go-hexagonal-architecture/application"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+}
