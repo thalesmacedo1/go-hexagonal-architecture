4afc4a549eb8d1ba62c9d0adc572e34824a0857b
Thales Macêdo <thalesmacedo1@live.com>
2022-07-23 21:25:59 -0300

    added isvalid method and tests

--- application/product_test.go
+++ application/product_test.go
@@ -3,6 +3,7 @@ package application_test
 import (
 	"testing"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"github.com/thalesmacedo1/go-hexagonal-architecture/application"
 )
@@ -33,4 +34,27 @@ func TestProduct_Disable(t *testing.T) {
 	product.Price = 10
 	err = product.Disable()
 	require.Equal(t, "price must zero in order to disable the product", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "price must be greater or equal zero", err.Error())
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/thalesmacedo1/go-hexagonal-architecture
 
 go 1.16
 
-require github.com/stretchr/testify v1.8.0
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/stretchr/testify v1.8.0
+)
